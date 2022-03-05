137442dd35294b87ed492114141af77734d9ee5c
Gonza Alecha <alechagonzalo@gmail.com>
2022-03-05 22:44:53 +0100

    add  product filter

--- src/pages/product-list/ProductList.jsx
+++ src/pages/product-list/ProductList.jsx
@@ -1,99 +1,69 @@
+import { useCallback, useEffect, useState } from "react";
+import { client } from "@/utils/api-client";
+
 import { ProductItem } from "@/components/ProductItem";
 import { Search } from "@/components/Search";
 import { Select } from "@/components/Select";
 import Skeleton from "@/components/Skeleton";
 import { GoTop } from "@/components/GoTop";
-import { device } from "@/styles/device";
-import { client } from "@/utils/api-client";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { ListContainer, NavBar, ProductsContainer } from "./styles";
+import { mapProductsAMtoProductsVM } from "@/utils/mapper";
 
-const ListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: ${({ theme }) => theme.spacing(2)};
-  width: auto;
+function filterProducts(products, filter) {
+  if (!products) return [];
 
-  @media ${device.tablet} {
-    width: 90%;
-  }
-`;
+  const pattern = filter
+    .split("")
+    .map((x) => {
+      return `(?=.*${x})`;
+    })
+    .join("");
+  const regex = new RegExp(`${pattern}`, "g");
+  return products.filter((prod) => {
+    return (
+      prod.brand.toLowerCase().match(regex) ||
+      prod.model.toLowerCase().match(regex)
+    );
+  });
+}
 
-const NavBar = styled.div`
-  margin-top: ${({ theme }) => theme.spacing(1)};
-  margin-bottom: ${({ theme }) => theme.spacing(4)};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: auto;
-  flex-direction: column-reverse;
-  gap: ${({ theme }) => theme.spacing(1)};
-  @media ${device.mobileL} {
-    flex-direction: row;
-  }
+let debounceTimer;
 
-  @media ${device.laptop} {
-    width: 90%;
-    justify-content: start;
-    margin-left: ${({ theme }) => theme.spacing(10)};
-    gap: ${({ theme }) => theme.spacing(4)};
-  }
-`;
-
-const ProductsContainer = styled.div`
-  display: grid;
-  margin-top: ${({ theme }) => theme.spacing(2)};
-  margin-bottom: ${({ theme }) => theme.spacing(4)};
-  justify-items: center;
-  grid-template-columns: repeat(1, 1fr);
-
-  @media ${device.mobileL} {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media ${device.tablet} {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media ${device.laptop} {
-    grid-template-columns: repeat(4, 1fr);
-  }
-`;
+const debounce = (callback, time) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(callback, time);
+};
 
 export const ProductList = () => {
   const [products, setProducts] = useState(null);
-  const [brands, setBrands] = useState([]);
+  const [filter, setFilter] = useState("");
 
   const loadData = async () => {
     const data = await client("api/product");
-    setProducts(data);
-
-    let brands = [];
-    let minPrice = data[0].price;
-    let maxPrice = 0;
-    data.forEach((p) => {
-      const price = Number(p.price);
-      brands.includes(p.brand.toUpperCase())
-        ? null
-        : brands.push(p.brand.toUpperCase());
-      minPrice = !!price && price < minPrice ? price : minPrice;
-      maxPrice = !!price && price > maxPrice ? price : maxPrice;
-    });
-    setBrands(brands);
+    setProducts(mapProductsAMtoProductsVM(data));
   };
 
   useEffect(() => {
     loadData();
   }, []);
 
+  function handleFilter(e) {
+    debounce(() => {
+      setFilter(e.target.value);
+    }, 500);
+  }
+
+  const filteredProducts = filterProducts(products, filter);
+
   return (
     <ListContainer>
       <NavBar>
-        <Select options={brands} />
-        <Search />
+        <Search onChange={handleFilter} />
       </NavBar>
       <ProductsContainer>
-        {products ? (
+        {filteredProducts ? (
           <>
-            {products.map((item) => {
+            {filteredProducts.map((item) => {
               return <ProductItem item={item} key={item.id} />;
             })}
           </>
